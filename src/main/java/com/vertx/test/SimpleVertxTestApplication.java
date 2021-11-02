2bd98401b4a138e56a0d2f79dc1b3a66d58f131d
DESKTOP-HL38RDE\USER <rhksdlr134@naver.com>
2021-11-02 18:09:13 +0900

    First Commit

---
+++ src/main/java/com/vertx/test/SimpleVertxTestApplication.java
@@ -0,0 +1,13 @@
+package com.vertx.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleVertxTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleVertxTestApplication.class, args);
+    }
+
+}
